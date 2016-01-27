bed2514a60f1f878f6ea1b1dcd09d2b9fe9ce780
emilycarolineking <emking25@gmail.com>
2016-01-26 22:58:11 -0500

    Person4.java file complete

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -9,42 +9,67 @@ package edu.gatech.oad.antlab.person;
  *  @version 1.1
  */
 public class Person4 {
-  /** Holds the persons real name */
+  /**
+   * Holds the persons real name
+   */
   private String name;
-    /**
-     * The constructor, takes in the persons
-     * name
-     * @param pname the person's real name
-     */
+
+  /**
+   * The constructor, takes in the persons
+   * name
+   *
+   * @param pname the person's real name
+   */
   public Person4(String pname) {
     name = pname;
   }
-    /**
-     * This method should return a string
-     * where each character is 1 greater 
-     * than its previous value.  So
-     * given "abc123" it should return
-     * "bcd234".
-     *
-     * @param input the string to be modified
-     * @return the modified string
-     */
-    private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
-    }
-    
-    /**
-     * Return a string rep of this object
-     * that varies with an input string
-     *
-     * @param input the varying string
-     * @return the string representing the 
-     *         object
-     */
-    public String toString(String input) {
-      return name + calc(input);
+
+  /**
+   * This method should return a string
+   * where each character is 1 greater
+   * than its previous value.  So
+   * given "abc123" it should return
+   * "bcd234".
+   *
+   * @param input the string to be modified
+   * @return the modified string
+   */
+  private String calc(String input) {
+    char[] c = input.toCharArray();
+    String out;
+    for (int i = 0; i < input.length(); i++) {
+      c[i]++;
     }
 
+    out = new String(c);
+
+    System.out.println(out);
+    return out;
+  }
+
+  /**
+   * Return a string rep of this object
+   * that varies with an input string
+   *
+   * @param input the varying string
+   * @return the string representing the
+   * object
+   */
+  public String toString(String input) {
+    return name + calc(input);
+  }
+
+
+  public static void main(String[] args) {
+    Person4 perp = new Person4("Joe");
+    //  Person4 perp2 = new Person4("Bob");
+    //  Person4 perp3 = new Person4("Sam");
+
+
+    perp.calc("hello");
+    perp.calc("abc123");
+    perp.calc("xyz789");
+
+  }
 }
 
